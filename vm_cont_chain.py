e18cdb88ed4e7ce06230ff90bd56932eb6055d08
cmbaker3 <123706574+cmbaker3@users.noreply.github.com>
2023-02-17 17:48:04 -0800

    Update vm_cont_chain.py

--- vm_cont_chain.py
+++ vm_cont_chain.py
@@ -2,12 +2,7 @@
 
 import paho.mqtt.client as mqtt
 import socket
-def call_back_pong(client, userdata, message):
-   num = int(message.payload.decode()) + 1;
-   print("Ping: " + num) 
-   time.sleep(4)
-   client.publish("cmbaker/pong", f"{num}")
-   print("Ponging Num!")
+
 
 def on_connect(client, userdata, flags, rc):
     # Subscribe to servers
@@ -16,15 +11,30 @@ def on_connect(client, userdata, flags, rc):
     
     #Add the custom callbacks by indicating the topic and the name of the callback handle
     client.message_callback_add("cmbaker/ping", call_back_pong)
+   
+def call_back_pong(client, userdata, message):
+    num = int(message.payload.decode()) + 1
+    print("Ping: " + num) 
+    time.sleep(4)
+    client.publish("cmbaker/pong", f"{num}")
+    print("Ponging Num!")
+      
+def on_message(client, userdata, msg):
+    print("Default callback - topic: " + msg.topic + "   msg: " + str(msg.payload, "utf-8"))
 
 if __name__ == '__main__':
     #create a client object
     client = mqtt.Client()
+    #attach a default callback which we defined above for incoming mqtt messages
+    client.on_message = on_message
+    #attach the on_connect() callback function defined above to the mqtt client
+    client.on_connect = on_connect
+
 
     #attach the on_connect() callback function defined above to the mqtt client
     client.on_connect = on_connect
           
-    client.connect(host="68.181.32.115", port=11000, keepalive=60)
+    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
     
     client.loop_forever()
     time.sleep(1)
